8524fb24902b985f980360aaa30456144a3aaa0a
Muntaser Syed <jemsbhai@gmail.com>
2020-07-26 09:07:38 -0400

    Create gethackers.py

---
+++ backend/gethackers.py
@@ -0,0 +1,94 @@
+import os
+import pymongo
+import json
+import random
+
+def dummy(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    if request.method == 'OPTIONS':
+        # Allows GET requests from origin https://mydomain.com with
+        # Authorization header
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Max-Age': '3600',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+        return ('', 204, headers)
+
+    # Set CORS headers for main requests
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+    }
+
+    request_json = request.get_json()
+    mongostr = os.environ.get('MONGOSTR')
+    client = pymongo.MongoClient(mongostr)
+    db = client["hackyteambuilder"]
+    col = db.hackers
+    results = []
+    maxid = 0
+    
+    if request_json['id'] == "-1":
+        for x in col.find():
+            # item = {}
+            # item['id'] = str(x['id'])
+            # item["emoji"] =  str(x["emoji"])
+            # item["description"] =  x["description"]
+            # item["name"] =  x["name"]
+            # item["lat"] =  x["lat"]
+            # item["long"] =  x["long"]
+            # item["img_url"] =  x["img_url"]
+            results.append(x['id'])
+            maxid +=1
+        retjson = {}
+
+        retjson['ids'] = results
+        retjson['mongoresult'] = str(maxid)
+
+        return json.dumps(retjson)
+
+
+    for x in col.find():
+        if x['id'] == request_json['id']:
+            continue
+        # item = {}
+        # item['id'] = str(x['id'])
+        # item["emoji"] =  str(x["emoji"])
+        # item["description"] =  x["description"]
+        # item["name"] =  x["name"]
+        # item["lat"] =  x["lat"]
+        # item["long"] =  x["long"]
+        # item["img_url"] =  x["img_url"]
+        results.append(x['id'])
+        maxid +=1
+
+
+    random.shuffle(results) 
+    # do the ml magic here
+        
+    retjson = {}
+
+    retjson['ids'] = results
+    retjson['mongoresult'] = str(maxid)
+
+    return json.dumps(retjson)
+
+
+    retstr = "action not done"
+
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return retstr
